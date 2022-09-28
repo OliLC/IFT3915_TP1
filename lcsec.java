ed2d981ac7990d8dbe35f69a2de5fdb67f2278f1
derrick-chung <derrickch007@gmail.com>
2022-09-28 12:50:19 -0400

    Created lcsec

---
+++ lcsec.java
@@ -0,0 +1,69 @@
+import java.io.*;
+
+public class lcsec {
+    public static void main(String[] args) {
+        produceCSVFile(args[0]);
+    }
+
+    public static int couplageSimpleEntreClasses(String fileName, String path) {
+        int counter = 0;
+        File parent = new File(path);
+        File[] children = parent.listFiles();
+        BufferedReader reader;
+        for (File i : children) {
+            if (i.isDirectory()) {
+                counter += couplageSimpleEntreClasses(fileName, path + "/" + i.getName());
+            } else {
+                try {
+                    reader = new BufferedReader(new FileReader(i.getName()));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        if(line.contains(fileName)){}
+                        counter++;
+                    }
+                    reader.close();
+
+                } catch (FileNotFoundException fnfe) {
+                    System.out.println("File not found");
+                    continue;
+                } catch (IOException e) {
+                    System.out.println("Empty line or file");
+                }
+            }
+        }
+        return counter;
+    }
+
+    public static void produceCSVFile(String path) {
+        String content = produceCSVContent(path);
+        jls fileProducer = new jls();
+        jls.produceCSVFile(content);
+    }
+
+    public static String produceCSVContent(String path) {
+        String outputCSV = "";
+        File parent = new File(path);
+        jls lineProducer = new jls();
+
+        if (!parent.isDirectory()) {
+            return outputCSV;
+        }
+
+
+        File[] children = parent.listFiles();
+
+        for (File i : children) {
+            if (i.isDirectory()) {
+                outputCSV = outputCSV + produceCSVContent(path + "/" + i.getName());
+            } else {
+                String childName = i.getName();
+                String outputLine = lineProducer.produceCSVLine(path, childName) + "," + couplageSimpleEntreClasses(childName, path);
+                outputCSV += outputLine + "\n";
+                System.out.println(outputLine);
+            }
+        }
+
+        return outputCSV;
+    }
+
+}
