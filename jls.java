a1cb9da4c8159df07a75c347a1810d91a91cec48
derrick-chung <derrickch007@gmail.com>
2022-09-28 10:40:27 -0400

    Update jls.java
    Made it more modular

--- jls.java
+++ jls.java
@@ -5,31 +5,32 @@ import java.io.FileNotFoundException;
 
 public class jls {
     public static void main(String[] args){
-        String cvsContent = produceCVS(args[0]);
-
+        String csvContent = produceCSVContent(args[0]);
+        produceCSVFile(csvContent);
+    }
+    public static void produceCSVFile(String content){
         try {
-            File CVSfile = new File("output.cvs");
-            CVSfile.createNewFile();
+            File CSVFile = new File("output.csv");
+            CSVFile.createNewFile();
         } catch (IOException e) {
             System.out.println("File creation ERROR");
         }
-        
+
         try {
-            FileWriter writer = new FileWriter("output.cvs");
-            writer.write(cvsContent);
+            FileWriter writer = new FileWriter("output.csv");
+            writer.write(content);
             writer.close();
         } catch (IOException e) {
             System.out.println("FILEWRITER error");
         }
     }
-    
 
-    public static String produceCVS(String path){
-        String outputCVS = "";
+    public static String produceCSVContent(String path){
+        String outputCSV = "";
         File parent = new File(path);
 
         if(!parent.isDirectory()){
-            return outputCVS;
+            return outputCSV;
         }
 
 
@@ -37,17 +38,22 @@ public class jls {
         
         for(File i : children){
             if (i.isDirectory()){
-                outputCVS = outputCVS + produceCVS(path + "/" + i.getName());
+                outputCSV = outputCSV + produceCSVContent(path + "/" + i.getName());
             }
             else{
-                String child = i.getName();
-                String output = path + "/" + child + ", " + getPackageNameFromPath(path) + ", " + child.substring(0, child.indexOf('.'));
-                outputCVS += output + "\n";
-                System.out.println(output);
+                String childName = i.getName();
+                String outputLine = produceCSVLine(path,childName);
+                outputCSV += outputLine + "\n";
+                System.out.println(outputLine);
             }
         }
 
-        return outputCVS;
+        return outputCSV;
+    }
+    public static String produceCSVLine(String path, String childName){
+
+        String outputLine = path + "/" + childName + ", " + getPackageNameFromPath(path) + ", " + childName.substring(0, childName.indexOf('.'));
+        return outputLine;
     }
     
     public static String getPackageNameFromPath(String path){
